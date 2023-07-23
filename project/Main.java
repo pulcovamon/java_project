fe2242f4c8b028a73940f1a24b6db0a55da2d42f
Monika <pulcovamon@gmail.com>
2023-07-23 17:31:37 +0200

    bonuses and critical success

--- project/Main.java
+++ project/Main.java
@@ -3,13 +3,19 @@ package project;
 public class Main {
     
     public static void main(String[] args) {
-        Monster snake = new Monster("Snake", 50, Dice.D8, 2);
+        Monster snake = new Monster(
+                                    "Snake",
+                                    50,
+                                    5,
+                                    Dice.D8, 
+                                    2,
+                                    4
+                                    );
 
         for (int i = 0; i < 3; i++) {
             if (snake.attack(15)) {
-                snake.damage();
+                snake.dealDamage();
             }
         }
     }
-
 }
--- project/Monster.java
+++ project/Monster.java
@@ -4,16 +4,28 @@ public class Monster {
     public String name;
     public int current_hp;
     private boolean living = true;
+    private int attack_bonus;
     Roll attack_roll = new Roll(Dice.D20, 1);
     Roll damage_roll;
+    private int damage_bonus;
+    private boolean critical = false;
 
-    public Monster(String name, int max_hp, Dice damage_dice, int number_of_dice) {
+    public Monster(
+                    String name,
+                    int max_hp,
+                    int attack_bonus,
+                    Dice damage_dice,
+                    int number_of_dice,
+                    int damage_bonus
+                    ) {
         this.name = name;
         this.current_hp = max_hp;
+        this.attack_bonus = attack_bonus;
         this.damage_roll = new Roll(damage_dice, number_of_dice);
+        this.damage_bonus = damage_bonus;
     }
 
-    public void demage(int minus_hp) {
+    public void getDamage(int minus_hp) {
         this.current_hp -= minus_hp;
 
         if (this.current_hp <= 0) {
@@ -28,19 +40,33 @@ public class Monster {
 
     public boolean attack(int target_ac) {
         int current_roll = this.attack_roll.useRoll();
+        int amount = current_roll + this.attack_bonus;
 
-        if (current_roll > target_ac) {
-            System.out.println("Success");
+        if (current_roll == 20) {
+            this.critical = true;
+            System.out.println("Critical success! " + current_roll + "+" + this.attack_bonus);
+            return true;
+        } else if (amount > target_ac) {
+            System.out.println("Success! " + current_roll + "+" + this.attack_bonus);
             return true;
         } else {
-            System.out.println("Failure");
+            System.out.println("Failure! " + current_roll + "+" + this.attack_bonus);
             return false;
         }
     }
 
-    public int damage() {
-        int amount =  this.damage_roll.useRoll();
-        System.out.println(amount);
+    public int dealDamage() {
+        int amount;
+        if (this.critical) {
+            amount = this.damage_bonus;
+            for (int i = 0; i < 2; i++) {
+                amount += this.damage_roll.useRoll();
+            }
+            this.critical = false;
+        } else {
+            amount =  this.damage_roll.useRoll() + this.damage_bonus;
+        }
+        System.out.println("Dealing " + amount + " damage");
         return amount;
     }
 }
